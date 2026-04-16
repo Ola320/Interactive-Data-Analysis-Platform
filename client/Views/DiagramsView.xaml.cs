36c9ac5a326d0617b70f2993aad5f16a53ac3bb4
Marysia <marysiad2005@wp.pl>
2026-04-16 12:56:09 +0200

    Diagrams View

---
+++ client/Views/DiagramsView.xaml.cs
@@ -0,0 +1,12 @@
+using System.Windows.Controls;
+
+namespace DataAnalizer.Views
+{
+    public partial class DiagramsView : UserControl
+    {
+        public DiagramsView()
+        {
+            InitializeComponent();
+        }
+    }
+}
